4b45fc057a3c77a58c6afee28d8443d1c336d9b0
sahinmelikov <1rima621melikov@gmail.com>
2023-05-09 03:16:45 +0400

    htmlcs

---
+++ LayOut_ViewStart/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayOut_ViewStart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
